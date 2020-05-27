e80645541c15e029164479c259ddb85b355848ae
NathanMagnan <nathan.magnan@polytechnique.edu>
2020-05-27 12:19:03 +0200

    wrote a script to analyse with GP only the degree statistic

---
+++ draw_figure_12_d.py
@@ -0,0 +1,165 @@
+## Imports
+import numpy as np
+import GPy as GPy
+import os
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('text', usetex = True)
+
+print("All imports successful")
+
+## Getting the data set
+print("starting to load the data")
+
+target = "/home/astro/magnan/Repository_Stage_3A/data_set_Abacus/data_set_Abacus"
+
+X_data = np.loadtxt(str(target) + "_X_data_d") # numpy array with fields h0, w0, ns, sigma8, omegaM, d -- 6 points per simu
+Y_data = np.loadtxt(str(target) + "_Y_data_d") # numpy array with field Nd
+
+print("data loaded")
+
+## Dividing the data set
+print("starting to get the Planck parameters")
+noise = 0.01
+
+X_planck = X_data[240:246]
+Y_planck = Y_data[240:246]
+
+h0_i, w0_i, ns_i, sigma8_i, omegaM_i = X_planck[5, 0:5]
+h0_p = h0_i  *  (75 - 60) + 60 # we had normalized every parameter between 0 and 1 when creating the data set
+w0_p = w0_i  *  ((-0.60) - (-1.40)) + (-1.40) # we had normalized every parameter between 0 and 1 when creating the data set
+ns_p = ns_i  *  (0.995 - 0.920) + 0.920 # we had normalized every parameter between 0 and 1 when creating the data set
+sigma8_p = sigma8_i  *  (1.04 - 0.64) + 0.64 # we had normalized every parameter between 0 and 1 when creating the data set
+omegaM_p = omegaM_i  *  (0.375 - 0.250) + 0.250 # we had normalized every parameter between 0 and 1 when creating the data set
+Planck_parameters = [h0_p, w0_p, ns_p, sigma8_p, omegaM_p]
+
+print("Planck parameters acquired")
+print(Planck_parameters)
+
+## Defining the grids
+print("starting to define the grids")
+
+# Coordinates
+H0 = np.linspace(60, 75, 100)
+W0 = np.linspace((-1.40), (-0.60), 100)
+Ns = np.linspace(0.920, 0.995, 100)
+Sigma8 = np.linspace(0.64, 1.04, 100)
+OmegaM = np.linspace(0.250, 0.375, 100)
+Coordinates = [H0, W0, Ns, Sigma8, OmegaM]
+Coordinates_names = ['$H_{0}$', '$w_{0}$', '$n_{s}$', '$\sigma_{8}$', '$\Omega_{M}$', '$\chi_{2}$']
+Coordinates_units = ['$km / s / Mpc$', 'a.u.', 'a.u.', 'a.u.', 'a.u.', 'a.u.']
+
+# Grids
+Grids = {'name_x_axis' : [], 'name_y_axis' : [], 'unit_x_axis' : [], 'unit_y_axis' : [], 'x_axis' : [], 'y_axis' : [], 'grid' : []}
+
+for i in range(5):
+    for j in range(i + 1):
+        Grids['name_x_axis'].append(Coordinates_names[i])
+        Grids['unit_x_axis'].append(Coordinates_units[i])
+        Grids['x_axis'].append(Coordinates[i])
+        
+        if (j == i):
+            Grids['name_y_axis'].append(Coordinates_names[5])
+            Grids['unit_y_axis'].append(Coordinates_units[5])
+            Grids['y_axis'].append([0])
+        else:
+            Grids['name_y_axis'].append(Coordinates_names[j])
+            Grids['unit_y_axis'].append(Coordinates_units[j])
+            Grids['y_axis'].append(Coordinates[j])
+        
+        X_axis = Grids['x_axis'][-1]
+        Y_axis = Grids['y_axis'][-1]
+        
+        if (j != i):
+            Grid = [[[0, 0, 0, 0, 0] for y in Y_axis] for x in X_axis]
+            for a in range(len(X_axis)):
+                for b in range(len(Y_axis)):
+                    Grid[a][b][i] = X_axis[a]
+                    Grid[a][b][j] = Y_axis[b]
+                    for c in range(5):
+                        if ((c != i) and (c != j)):
+                            Grid[a][b][c] = Planck_parameters[c]
+            Grid = np.asarray(Grid)
+            Grid = np.reshape(Grid, (len(X_axis) * len(Y_axis), 5))
+            
+        else:
+            Grid = [[0, 0, 0, 0, 0] for x in X_axis]
+            for a in range(len(X_axis)):
+                Grid[a][i] = X_axis[a]
+                for b in range(5):
+                    if (b != i):
+                        Grid[a][b] = Planck_parameters[b]
+        
+        Grids['grid'].append(Grid)
+
+print("grids defined")
+
+## Drawing
+print("starting to plot the results")
+
+K = [[0, 0, 0, 0, 0], [1, 2, 0, 0, 0], [3, 4, 5, 0, 0], [6, 7, 8, 9, 0], [10, 11, 12, 13, 14]]
+Coordinates_limits = [[60, 75], [-1.40, -0.60], [0.920, 0.995], [0.64, 1.04], [0.250, 0.375], [0, 1]]
+target = "/home/astro/magnan/Repository_Stage_3A/data_figure_12/figure_12"
+
+fig, axes = plt.subplots(nrows = 5, ncols =5, figsize = (20, 20))
+plt.subplots_adjust(hspace = 0.3, wspace = 0.4)
+
+for i in range(5):
+    for j in range(i+1):
+        subplot = axes[i][j]
+        k = K[i][j]
+        
+        try:
+            Grid_green_X = np.loadtxt(str(target) + "_grid_" + str(k) + "_X_green_d")
+        except:
+            Grid_green_X = []
+        try:
+            Grid_green_Y = np.loadtxt(str(target) + "_grid_" + str(k) + "_Y_green_d")
+        except:
+            Grid_green_Y = []
+        try:
+            Grid_red_X = np.loadtxt(str(target) + "_grid_" + str(k) + "_X_red_d")
+        except:
+            Grid_red_X = []
+        try:
+            Grid_red_Y = np.loadtxt(str(target) + "_grid_" + str(k) + "_Y_red_d")
+        except:
+            Grid_red_Y = []
+        
+        if (j == i):
+            if (i == 4):
+                subplot.set_xlabel(Grids['name_x_axis'][k] + ' [' + Grids['unit_x_axis'][k] + ']')
+            subplot.set_ylabel(Grids['name_y_axis'][k] + ' [' + Grids['unit_y_axis'][k] + ']')
+            subplot.set_xlim(left = Coordinates_limits[i][0], right = Coordinates_limits[i][1])
+            subplot.set_ylim(bottom = Coordinates_limits[5][0], top = Coordinates_limits[5][1])
+            if (Grid_green_X != []):
+                subplot.plot(Grid_green_X, Grid_green_Y, 'r')
+            if (Grid_red_X != []):
+                subplot.plot(Grid_red_X, Grid_red_Y, 'g')
+            subplot.axvline(x = Planck_parameters[i], color = 'black', linestyle = '--')
+        else:
+            if (i == 4):
+                subplot.set_xlabel(Grids['name_y_axis'][k] + ' [' + Grids['unit_y_axis'][k] + ']')
+            if (j == 0):
+                subplot.set_ylabel(Grids['name_x_axis'][k] + ' [' + Grids['unit_x_axis'][k] + ']')
+            subplot.set_xlim(left = Coordinates_limits[j][0], right = Coordinates_limits[j][1])
+            subplot.set_ylim(bottom = Coordinates_limits[i][0], top = Coordinates_limits[i][1])
+            if (Grid_green_X != []):
+                subplot.scatter(Grid_green_X, Grid_green_Y, s = 1, c = 'red')
+            if (Grid_red_X != []):
+                subplot.scatter(Grid_red_X, Grid_red_Y, s = 1, c = 'green')
+            subplot.axvline(x = Planck_parameters[j], color = 'black', linestyle = '--')
+            subplot.axhline(y = Planck_parameters[i], color = 'black', linestyle = '--')
+            
+    for j in range(i+1, 5):
+        subplot = axes[i][j]
+        subplot.axis('off')
+
+plt.suptitle("Sensitivity of the degree $d$ to cosmological parameters")
+print("results plotted")
+
+print("starting to save the results")
+my_path = os.path.abspath('/home/astro/magnan/Repository_Stage_3A/Figures')
+my_file = 'Sensitivity_MST_Abacus_d'
+plt.savefig(os.path.join(my_path, my_file))
+print("results saved")
---
+++ find_best_kernel_d.py
@@ -0,0 +1,161 @@
+""" need to modify GP_tools """
+## Imports
+import numpy as np
+import GPy as GPy
+import matplotlib.pyplot as plt
+
+import sys
+import os
+sys.path.append('/home/astro/magnan/Repository_Stage_3A')
+import GP_tools_d as GP
+os.chdir('/home/astro/magnan')
+
+print("All imports successful")
+
+## Importing the data
+print("Connexion successfull")
+print("starting to load the data")
+
+target = "/home/astro/magnan/Repository_Stage_3A/data_set_Abacus/data_set_Abacus"
+
+X_data = np.loadtxt(str(target) + "_X_data_d") # numpy array with fields h0, w0, ns, sigma8, omegaM, ds -- 6 point per simu
+Y_data = np.loadtxt(str(target) + "_Y_data_d") # numpy array with field Nd
+
+X_data = X_data[0:240] # we leave the planck simulation out
+Y_data = Y_data[0:240]
+
+print("data loaded")
+
+## Setting up the kernels to study
+print("starting to define the kernels")
+
+Kernels = []
+Kernel_names = []
+
+# RBF
+kernel = GPy.kern.RBF(6, active_dims = [0, 1, 2, 3, 4, 5])
+Kernels.append(kernel)
+Kernel_names.append('RBF isotropic')
+
+# Exponential
+kernel = GPy.kern.Exponential(6, active_dims = [0, 1, 2, 3, 4, 5])
+Kernels.append(kernel)
+Kernel_names.append('Exponential')
+
+# Matern 3/2
+kernel = GPy.kern.Matern32(6, active_dims = [0, 1, 2, 3, 4, 5])
+Kernels.append(kernel)
+Kernel_names.append('Matern32')
+
+# Matern 5/2
+kernel = GPy.kern.Matern52(6, active_dims = [0, 1, 2, 3, 4, 5])
+Kernels.append(kernel)
+Kernel_names.append('Matern52')
+
+# RBF
+kernel = GPy.kern.RBF(6, active_dims = [0, 1, 2, 3, 4, 5], ARD = True)
+Kernels.append(kernel)
+Kernel_names.append('RBF anisotropic')
+
+# RBF
+kernel = GPy.kern.RBF(6, active_dims = [0, 1, 2, 3, 4, 5], ARD = True)
+Kernels.append(kernel)
+Kernel_names.append('RBF anisotropic bounded')
+
+# RBF
+kernel = GPy.kern.RBF(6, active_dims = [0, 1, 2, 3, 4, 5], ARD = True)
+Kernels.append(kernel)
+Kernel_names.append('RBF anisotropic with prior')
+
+# RBF
+kernel = GPy.kern.RBF(6, active_dims = [0, 1, 2, 3, 4, 5], ARD = True)
+Kernels.append(kernel)
+Kernel_names.append('RBF anisotropic sgc')
+
+# RBF
+kernel = GPy.kern.RBF(6, active_dims = [0, 1, 2, 3, 4, 5], ARD = True)
+Kernels.append(kernel)
+Kernel_names.append('RBF anisotropic restart')
+
+print("kernels defined")
+
+## Setting up the data and test groups
+n_points_per_simulation = 6
+n_simulations = 40
+n_groups = 10
+
+List_groups = []
+for i in range(n_groups):
+   start_group = (i * n_simulations // n_groups) * n_points_per_simulation
+   end_group =  ((i + 1) * n_simulations // n_groups) * n_points_per_simulation
+   
+   X_test_group = X_data[start_group:end_group]
+   X_data_group = np.concatenate((X_data[0:start_group], X_data[end_group:]), 0)
+   Y_test_group = Y_data[start_group:end_group]
+   Y_data_group = np.concatenate((Y_data[0:start_group], Y_data[end_group:]), 0)
+   
+   List_groups.append((X_data_group, Y_data_group, X_test_group, Y_test_group))
+
+print("data and test groups defined")
+
+## Evaluating the kernels performances
+print("starting to evaluate the performances")
+
+Performances = []
+
+for k in range(len(Kernels)):
+    print("starting to work on kernel " + str(k))
+    
+    kernel = Kernels[k]
+    mean = 0
+    std = 0
+    
+    for i in range(n_groups):
+        print("kernel " + str(k) + " group " + str(i))
+        
+        # getting the right data and test groups
+        X_data_group, Y_data_group, X_test_group, Y_test_group = List_groups[i]
+        
+        # modelling the noise
+        noise_data = 0.01
+        noise_test = 0.01
+        
+        # creating the gaussian process and optimizing it
+        gp = GP.GP(X_data_group, Y_data_group, kernel = kernel, noise_data = noise_data)
+        gp.initialise_model()
+        
+        if (k <= 4):
+            gp.optimize_model()
+        if (k == 5):
+            gp.model.mul.rbf.lengthscale.constrain_bounded(0, 3)
+            gp.optimize_model()
+        if (k == 6):
+            gp.model.mul.rbf.lengthscale.set_prior(GPy.priors.Gamma(1, 1))
+            gp.optimize_model()
+        if (k == 7):
+            gp.optimize_model('scg')
+        if (k == 8):
+            gp.model.optimize_restart()
+        
+        # printing results of optimisation
+        if (k >= 4):
+            print(gp.model.mul.rbf.variance)
+            print(gp.model.mul.rbf.lengthscale)
+            print(gp.model.Gaussian_noise.variance)
+        
+        # getting the performance of the gaussian process
+        performance_new = gp.compute_performance_on_tests(X_test = X_test_group, Y_test = Y_test_group, noise_test = noise_test)
+        
+        mean_old = mean
+        std_old = std
+        mean_new = (i * mean_old + performance_new) / (i + 1)
+        std_new = np.sqrt((i * (std_old**2 + mean_old**2) + performance_new**2) / (i + 1) - mean_new**2)
+        mean = mean_new
+        std = std_new
+    
+    Performances.append((mean, std))
+    print("work done on kernel " + str(k))
+
+print("performances successfully evaluated")
+print(Performances)
+print(Kernel_names)
