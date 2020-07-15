60b8fec4d6715009d28855732717d69d971dd51b
NathanMagnan <nathan.magnan@polytechnique.edu>
2020-07-15 07:59:10 +0200

    drew the effects of masks on abacus and random catalogue. There was no effect. I suspect an error in the script

--- Draw_effect_masks.py
+++ Draw_effect_masks.py
@@ -44,12 +44,12 @@ print("Abacus histograms loaded")
 print("starting to plot")
 
 my_path = os.path.abspath('/home/astro/magnan/Repository_Stage_3A/Figures')
-my_file = 'Effect_masks_abacus.png'
+my_file = 'Effect_masks_random.png'
 my_file = os.path.join(my_path, my_file)
 
 plot_histograms = mist.PlotHistMST()
 for m in range(4):
-    plot_histograms.read_mst(Abacus_masked_hists[m], label = Labels[m])
+    plot_histograms.read_mst(Random_masked_hists[m], label = Labels[m])
 plot_histograms.plot(usecomp = True, usemean = False, whichcomp = 3, figsize = (9, 6), saveas = my_file)
 
 print("Statistics plotted")
